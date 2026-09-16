dbac72dc734a249aba4b12d7d729e849f804180d
RedZone02 <felescorobertvalentin@gmail.com>
2026-09-16 17:56:37 +0300

    feat: add allowedDevOrigins to next configuration

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  allowedDevOrigins: ["169.254.83.107"],
+};
 
 export default nextConfig;
